e9ca827bb1edb49bcf221490fa26c4c1f4f72421
Shamim rahman khan <78911344+vividblueprint@users.noreply.github.com>
2024-03-31 16:20:52 +0800

    remove env

--- resources/ros_noetic.sh
+++ resources/ros_noetic.sh
@@ -69,22 +69,22 @@ catkin_init_workspace
 cd $HOME/$name_catkin_workspace
 catkin_make
 
-echo "[Set the ROS evironment]"
-sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
-sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
-sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
-sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
-sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
+# echo "[Set the ROS evironment]"
+# sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
+# sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
+# sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
+# sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+# sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
+# sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
+# sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
 
-sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+# sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+# sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
 
-sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
-sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
+# sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
+# sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
 
-source $HOME/.bashrc
+# source $HOME/.bashrc
 
 echo "[Complete!!!]"
 exit 0
